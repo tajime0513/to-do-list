fdd64c7885d144e7c8ad1a4d3db9cec0332515bd
tajime0513 <tajime0513@gmail.com>
2024-07-22 02:34:48 +0900

    feat: submitTodo 함수 생성
    - 초기 함수 생성

--- js/submitTodo.js
+++ js/submitTodo.js
@@ -1,1 +1,11 @@
 import {temporaryStorageTodo} from './createTodo.js'
+
+const formEl = document.querySelector('form');
+
+ const submitTodo = (event) => {
+    event.preventDefault();
+  console.log('sumbitTodo')
+
+}
+
+export {submitTodo}
