fd1de94da15b3061771fddd8b04e610ddf85a95a
tajime0513 <tajime0513@gmail.com>
2024-07-22 05:40:30 +0900

    feat: submitTodo 함수 생성

--- js/submitTodo.js
+++ js/submitTodo.js
@@ -1,11 +1,18 @@
-import {temporaryStorageTodo} from './createTodo.js'
+import { temporaryStorageTodo } from './createTodo.js';
+import { restInputFields } from './resetInputFields.js';
 
 const formEl = document.querySelector('form');
 
- const submitTodo = (event) => {
+const submitTodo = () => {
+  formEl.addEventListener('submit', (event) => {
     event.preventDefault();
-  console.log('sumbitTodo')
+    const formData = new FormData();
+    const tempTodo = temporaryStorageTodo();
+    for (let key in tempTodo) {
+      formData.append(key, tempTodo[key]);
+    }
+    restInputFields();
+  });
+};
 
-}
-
-export {submitTodo}
+export { submitTodo };
